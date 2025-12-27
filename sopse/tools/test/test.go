9a5d8fa640d4fcc29f535f0149838f79905d9c1b
Stephen Malone <mail@wirehaiku.org>
2025-12-27 17:56:00 +1000

    Added database functions to tools/test.

--- sopse/tools/test/test.go
+++ sopse/tools/test/test.go
@@ -1,1 +1,41 @@
 package test
+
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stvmln86/sopse/sopse/tools/sqls"
+)
+
+// MockData is additional database data for unit testing.
+const MockData = `
+	insert into Users (init, uuid, addr) values (1000, '1111', '1.1.1.1');
+	insert into Pairs (init, user, name, body) values
+		(1000, 1, 'alpha', 'Alpha.' || char(10)),
+		(2000, 1, 'bravo', 'Bravo.' || char(10));
+`
+
+// Get returns the first value from a database query.
+func Get(t *testing.T, db *sqlx.DB, code string, elems ...any) any {
+	var data any
+	if err := db.Get(&data, code, elems...); err != nil {
+		t.Fatal(err)
+	}
+
+	return data
+}
+
+// MockDB returns an in-memory database populated with MockData.
+func MockDB(t *testing.T) *sqlx.DB {
+	db, err := sqlx.Connect("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := db.Exec(sqls.Pragma + sqls.Schema + MockData); err != nil {
+		t.Fatal(err)
+	}
+
+	return db
+}
--- sopse/tools/test/test_test.go
+++ sopse/tools/test/test_test.go
@@ -1,1 +1,27 @@
 package test
+
+import (
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGet(t *testing.T) {
+	// setup
+	db, err := sqlx.Connect("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// success
+	data := Get(t, db, "select ?", 123)
+	assert.Equal(t, int64(123), data)
+}
+
+func TestMockDB(t *testing.T) {
+	// success
+	db := MockDB(t)
+	assert.NotNil(t, db)
+}
