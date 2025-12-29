6fc744a5c00a3a183c197f6aa9e18b96cd56cb3e
Stephen Malone <mail@wirehaiku.org>
2025-12-29 12:54:50 +1000

    Added items/pair package.

---
+++ sopse/items/pair/pair.go
@@ -0,0 +1,78 @@
+// Package pair implements the Pair type and methods.
+package pair
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/stvmln86/sopse/sopse/tools/bolt"
+	"github.com/stvmln86/sopse/sopse/tools/neat"
+	"go.etcd.io/bbolt"
+)
+
+// Pair is a single recorded key-value pair.
+type Pair struct {
+	DB   *bbolt.DB
+	Addr string
+	Body string
+	Init time.Time
+}
+
+// New returns a new Pair.
+func New(db *bbolt.DB, addr, body string, init time.Time) *Pair {
+	return &Pair{db, addr, body, init}
+}
+
+// Get returns an existing Pair.
+func Get(db *bbolt.DB, addr string) (*Pair, error) {
+	bmap, err := bolt.Get(db, addr)
+	switch {
+	case bmap == nil:
+		return nil, nil
+	case err != nil:
+		return nil, fmt.Errorf("cannot get Pair %q - %w", addr, err)
+	}
+
+	init := neat.Time(bmap["init"])
+	return New(db, addr, bmap["body"], init), nil
+}
+
+// Set sets and returns a new or existing Pair.
+func Set(db *bbolt.DB, addr, body string, init time.Time) (*Pair, error) {
+	pair := New(db, addr, body, init)
+	if err := bolt.Set(db, addr, pair.Map()); err != nil {
+		return nil, fmt.Errorf("cannot set Pair %q - %w", addr, err)
+	}
+
+	return pair, nil
+}
+
+// Delete deletes the Pair.
+func (p *Pair) Delete() error {
+	if err := bolt.Delete(p.DB, p.Addr); err != nil {
+		return fmt.Errorf("cannot delete Pair %q - %w", p.Addr, err)
+	}
+
+	return nil
+}
+
+// Map returns the Pair as a string map.
+func (p *Pair) Map() map[string]string {
+	return map[string]string{
+		"body": p.Body,
+		"init": neat.Unix(p.Init),
+	}
+}
+
+// Name returns the Pair's base name.
+func (p *Pair) Name() string {
+	elems := strings.Split(p.Addr, ".")
+	return elems[len(elems)-1]
+}
+
+// Update overwrites the Pair with a new body.
+func (p *Pair) Update(body string) error {
+	p.Body = body
+	return bolt.Set(p.DB, p.Addr, p.Map())
+}
---
+++ sopse/items/pair/pair_test.go
@@ -0,0 +1,122 @@
+package pair
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stvmln86/sopse/sopse/tools/test"
+	"go.etcd.io/bbolt"
+)
+
+var time1000 = time.Unix(1000, 0).Local()
+
+func mockPair(t *testing.T) *Pair {
+	db := test.DB(t)
+	pair, err := Get(db, "pair.mockUser1.alpha")
+	test.Try(t, err)
+	return pair
+}
+
+func TestNew(t *testing.T) {
+	// setup
+	db := test.DB(t)
+
+	// success
+	pair := New(db, "addr", "body", time1000)
+	assert.Equal(t, db, pair.DB)
+	assert.Equal(t, "addr", pair.Addr)
+	assert.Equal(t, "body", pair.Body)
+	assert.Equal(t, time1000, pair.Init)
+}
+
+func TestGet(t *testing.T) {
+	// setup
+	db := test.DB(t)
+
+	// success
+	pair, err := Get(db, "pair.mockUser1.alpha")
+	assert.Equal(t, db, pair.DB)
+	assert.Equal(t, "pair.mockUser1.alpha", pair.Addr)
+	assert.Equal(t, "Alpha.", pair.Body)
+	assert.Equal(t, time1000, pair.Init)
+	assert.NoError(t, err)
+}
+
+func TestSet(t *testing.T) {
+	// setup
+	db := test.DB(t)
+
+	// success
+	pair, err := Set(db, "pair.mockUser1.alpha", "body", time1000)
+	assert.Equal(t, db, pair.DB)
+	assert.Equal(t, "pair.mockUser1.alpha", pair.Addr)
+	assert.Equal(t, "body", pair.Body)
+	assert.Equal(t, time1000, pair.Init)
+	assert.NoError(t, err)
+
+	// confirm - database
+	test.Try(t, db.View(func(tx *bbolt.Tx) error {
+		buck := tx.Bucket([]byte("pair.mockUser1.alpha"))
+		for attr, want := range pair.Map() {
+			data := string(buck.Get([]byte(attr)))
+			assert.Equal(t, want, data)
+		}
+
+		return nil
+	}))
+}
+
+func TestDelete(t *testing.T) {
+	// setup
+	pair := mockPair(t)
+
+	// success
+	err := pair.Delete()
+	assert.NoError(t, err)
+
+	// confirm - database
+	test.Try(t, pair.DB.View(func(tx *bbolt.Tx) error {
+		buck := tx.Bucket([]byte("pair.mockUser1.alpha"))
+		assert.Nil(t, buck)
+		return nil
+	}))
+}
+
+func TestMap(t *testing.T) {
+	// setup
+	pair := mockPair(t)
+
+	// success
+	bmap := pair.Map()
+	assert.Equal(t, map[string]string{
+		"body": "Alpha.",
+		"init": "1000",
+	}, bmap)
+}
+
+func TestName(t *testing.T) {
+	// setup
+	pair := mockPair(t)
+
+	// success
+	name := pair.Name()
+	assert.Equal(t, "alpha", name)
+}
+
+func TestUpdate(t *testing.T) {
+	// setup
+	pair := mockPair(t)
+
+	// success
+	err := pair.Update("body")
+	assert.NoError(t, err)
+
+	// confirm - database
+	test.Try(t, pair.DB.View(func(tx *bbolt.Tx) error {
+		buck := tx.Bucket([]byte("pair.mockUser1.alpha"))
+		body := string(buck.Get([]byte("body")))
+		assert.Equal(t, "body", body)
+		return nil
+	}))
+}
