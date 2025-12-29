4e2b74408169841d5f67fb4cd79141f5390df6bc
Stephen Malone <mail@wirehaiku.org>
2025-12-29 10:16:59 +1000

    Added error handling to tools/test.

--- sopse/tools/test/test.go
+++ sopse/tools/test/test.go
@@ -15,32 +15,45 @@ var mockData = map[string]map[string]string{
 	"pair.mockUser1.bravo": {"body": "Bravo.", "init": "1100"},
 }
 
+// DB returns a temporary database containing mockData.
+func DB(t *testing.T) *bbolt.DB {
+	dire := t.TempDir()
+	dest := filepath.Join(dire, "mock.db")
+	db, err := bbolt.Open(dest, 0644, nil)
+	Try(t, err)
+
+	for name, pairs := range mockData {
+		for attr, data := range pairs {
+			Set(t, db, name, attr, data)
+		}
+	}
+
+	return db
+}
+
 // Get returns a database value.
-func Get(db *bbolt.DB, name, attr string) string {
+func Get(t *testing.T, db *bbolt.DB, name, attr string) string {
 	var data string
-	db.View(func(tx *bbolt.Tx) error {
+	Try(t, db.View(func(tx *bbolt.Tx) error {
 		data = string(tx.Bucket([]byte(name)).Get([]byte(attr)))
 		return nil
-	})
+	}))
 
 	return data
 }
 
-// MockDB returns a temporary database containing mockData.
-func MockDB(t *testing.T) *bbolt.DB {
-	dire := t.TempDir()
-	dest := filepath.Join(dire, "mock.db")
-	db, _ := bbolt.Open(dest, 0644, nil)
-	db.Update(func(tx *bbolt.Tx) error {
-		for name, pairs := range mockData {
-			buck, _ := tx.CreateBucketIfNotExists([]byte(name))
-			for attr, data := range pairs {
-				buck.Put([]byte(attr), []byte(data))
-			}
-		}
-
+// Set sets a new or existing database value.
+func Set(t *testing.T, db *bbolt.DB, name, attr, data string) {
+	Try(t, db.Update(func(tx *bbolt.Tx) error {
+		buck, _ := tx.CreateBucketIfNotExists([]byte(name))
+		buck.Put([]byte(attr), []byte(data))
 		return nil
-	})
+	}))
+}
 
-	return db
+// Try fails a test on a non-nil error.
+func Try(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
 }
--- sopse/tools/test/test_test.go
+++ sopse/tools/test/test_test.go
@@ -6,25 +6,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGet(t *testing.T) {
-	// setup
-	db := MockDB(t)
-
+func TestDB(t *testing.T) {
 	// success
-	data := Get(db, "user.mockUser1", "init")
-	assert.Equal(t, "1000", data)
-}
-
-func TestMockDB(t *testing.T) {
-	// success
-	db := MockDB(t)
+	db := DB(t)
 	assert.NotNil(t, db)
 
 	// confirm - mockData
 	for name, pairs := range mockData {
 		for attr, want := range pairs {
-			data := Get(db, name, attr)
+			data := Get(t, db, name, attr)
 			assert.Equal(t, want, data)
 		}
 	}
+}
+
+func TestGet(t *testing.T) {
+	// setup
+	db := DB(t)
+
+	// success
+	data := Get(t, db, "user.mockUser1", "init")
+	assert.Equal(t, "1000", data)
+}
+
+func TestSet(t *testing.T) {
+	// setup
+	db := DB(t)
+
+	// success
+	Set(t, db, "name", "attr", "data")
+	data := Get(t, db, "name", "attr")
+	assert.Equal(t, "data", data)
+}
+
+func TestTry(t *testing.T) {
+	// success
+	Try(t, nil)
 }
